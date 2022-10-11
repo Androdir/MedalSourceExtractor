acb6cfdb1896996599fa3fa36fcc53a2eec939b8
Androdir <66904930+Androdir@users.noreply.github.com>
2022-10-11 19:04:06 +0200

    Update

--- app.py
+++ app.py
@@ -1,17 +1,57 @@
 import requests
 import bs4
 import flask
+import threading
+from selenium import webdriver
+from selenium.webdriver.firefox.service import Service
+from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+import time
 
 app = flask.Flask(__name__)
 
+class Browser(threading.Thread):
+	def __init__(self, thread_name="browser", thread_id="browser"):
+		threading.Thread.__init__(self)
+		self.thread_name = thread_name
+		self.thread_id = thread_id
+
+		options = Options()
+		options.add_argument("--headless")
+		options.add_argument("--disable-gpu")
+		service = Service("geckodriver.exe")
+		self.driver = webdriver.Firefox(options=options, service=service)
+		
+		self.waiting = False
+
+	def open_page(self, url):
+		self.driver.get(url)
+
+	def wait_for(self, css_selector):
+		self.waiting = True
+		WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, css_selector)))
+		self.waiting = False
+
+	def get_source(self):
+		return self.driver.page_source
+
+browser = Browser()
 
 @app.route("/")
 def index():
 	return flask.render_template("index.html")
 
 
+extracting = False
 @app.route("/api/extract", methods=["POST"])
 def extract():
+	global extracting
+	if extracting:
+		return flask.jsonify({"error": "Already extracting"})
+
+	extracting = True
 	url = flask.request.headers.get("url")
 	if not url:
 		return flask.jsonify({"error": "URL not provided."})
@@ -23,13 +63,21 @@ def extract():
 	except Exception as e:
 		return flask.jsonify({"error": "Invalid URL."})
 
-	html = requests.get(url, allow_redirects=True).text
-	soup = bs4.BeautifulSoup(html, "html.parser")
-	script = soup.find("script").text
-	contentIndex = script.find("contentUrl")
-	commaIndex = script.find(",", contentIndex)
-	return flask.jsonify(script[contentIndex:commaIndex].replace("contentUrl", "").replace(":", "").replace('"', "").strip())
+	browser.open_page(url)
+	browser.wait_for("#feed-clip-player-DwYehifHv382K-\:r1\:-player > div:nth-child(2) > div:nth-child(4)")
+	while browser.waiting:
+		time.sleep(0.1)
+		app.logger.info("waiting.")
+	html = browser.get_source()
+	soup = str(bs4.BeautifulSoup(html, "html.parser"))
+	contentIndex = soup.find("contentUrl")
+	commaIndex = soup.find(",", contentIndex)
+	source = soup[contentIndex:commaIndex].replace("contentUrl", "").replace(":", "").replace('"', "").replace("https", "https:").strip()
+	extracting = False
+	if source == "":
+		app.logger.info(soup)
+	return flask.jsonify(source)
 
 
 if __name__ == "__main__":
-	app.run(debug=True)
+	app.run(debug=True, port=5003)
--- static/index.js
+++ static/index.js
@@ -1,14 +1,24 @@
-$("button").on("click", (e) => {
+$("#submit").on("click", e => {
+	$("#submit").attr("disabled", true);
+	$("#video-container").html(`<div class="spinner-border text-primary">`);
+	$("#extracted-url").val("");
 	fetch("/api/extract", {
 		method: "POST",
 		headers: {
 			"Content-Type": "application/json",
 			"Accept": "application/json",
-			"url": $("input").val()
+			"url": $("#url-input").val()
 		},
 	}).then(response => {
 		response.json().then(data => {
-			$("#pp").html(data);
+			$("#submit").attr("disabled", false);
+			$("video").remove();
+			$("#video-container").html(`<video src="${data}" type="video/mp4" controls></video>`);
+			$("#extracted-url").val(data);
 		})
 	});
 });
+
+$("#copy").on("click", e => {
+	navigator.clipboard.writeText($("#extracted-url").val());
+})
